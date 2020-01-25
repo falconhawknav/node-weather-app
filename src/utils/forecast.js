3c8128dbef1c42d966ff1d93b6da1b1fcfe2b69e
falconhawknav <falconhawk.nav@gmail.com>
2020-01-25 13:00:16 +0530

    Initial Commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,22 @@
+const request = require('request');
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'https://api.darksky.net/forecast/fa713f2b04f8bd81d893cafe16f87a03/' + latitude + ',' + longitude;
+    request({url, json: true}, (error, { body }) => {
+        if(error) {
+            callback('Unable to connect to weather service!', undefined)
+        } else if(body.error){
+            callback('Unable to find location', undefined)
+        } else {
+            const {temperature, precipProbability, timezone} = body.currently;
+            callback(undefined, {
+                temperature,
+                precipProbability,
+                location: timezone
+            })
+        }
+    })
+
+}
+
+module.exports = forecast
